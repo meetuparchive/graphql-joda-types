56950a977a4d084b20796cc023b39e6230a1b987
Andy Stanberry <astanberry@meetup.com>
2019-12-11 11:01:06 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
-'use strict';
-exports.__esModule = true;
-var graphql_1 = require('graphql');
-var joda = require('js-joda');
-var jodaToGraphql = function(name, description, jodaType, parser) {
-  var parse = function(value) {
+import { GraphQLScalarType, Kind, GraphQLScalarTypeConfig } from 'graphql';
+import * as joda from 'js-joda';
+
+export interface Type<T> extends Function {
+  new (...args: any[]): T;
+}
+
+const jodaToGraphql = <T>(
+  name: string,
+  description: string,
+  jodaType: Type<T>,
+  parser: (string) => T
+) => {
+  const parse = value => {
     try {
       return parser(value);
     } catch (e) {
       throw new TypeError(
-        name + ' can not represent an invalid string ' + value
+        `${name} can not represent an invalid string ${value}`
       );
     }
   };
-  var config = {
-    name: name,
-    description: description,
-    serialize: function(value) {
+
+  const config: GraphQLScalarTypeConfig<T, string> = {
+    name,
+    description,
+    serialize: (value): string => {
       if (value instanceof jodaType) {
         return value.toString();
       }
@@ -24,55 +33,57 @@ var jodaToGraphql = function(name, description, jodaType, parser) {
         return value;
       }
       throw new TypeError(
-        name + ' can not represent invalid type ' + JSON.stringify(value)
+        `${name} can not represent invalid type ${JSON.stringify(value)}`
       );
     },
-    parseValue: function(value) {
+    parseValue: (value): T => {
       if (typeof value === 'string') {
         return parse(value);
       }
       throw new TypeError(
-        name + ' can not represent non-string type ' + JSON.stringify(value)
+        `${name} can not represent non-string type ${JSON.stringify(value)}`
       );
     },
-    parseLiteral: function(ast) {
-      if (ast.kind !== graphql_1.Kind.STRING) {
+    parseLiteral: (ast): T => {
+      if (ast.kind !== Kind.STRING) {
         throw new TypeError(
-          name +
-            ' can not represent non-string type ' +
-            String(ast.value != null ? ast.value : null)
+          `${name} can not represent non-string type ${String(
+            ast.value != null ? ast.value : null
+          )}`
         );
       }
       return parse(ast.value);
     },
   };
-  return new graphql_1.GraphQLScalarType(config);
+
+  return new GraphQLScalarType(config);
 };
-exports.LocalDate = jodaToGraphql(
+
+export const LocalDate = jodaToGraphql(
   'LocalDate',
   'A date string, such as 2018-07-01, serialized in ISO8601 format',
   joda.LocalDate,
   joda.LocalDate.parse
 );
-exports.LocalDateTime = jodaToGraphql(
+export const LocalDateTime = jodaToGraphql(
   'LocalDateTime',
   'A date and time, such as 2018-07-01T12:00:00, without a timezone, serialized in ISO8601 format',
   joda.LocalDateTime,
   joda.LocalDateTime.parse
 );
-exports.LocalTime = jodaToGraphql(
+export const LocalTime = jodaToGraphql(
   'LocalTime',
   'A time string, such as 12:00:00, serialized in ISO8601 format',
   joda.LocalTime,
   joda.LocalTime.parse
 );
-exports.ZonedDateTime = jodaToGraphql(
+export const ZonedDateTime = jodaToGraphql(
   'ZonedDateTime',
   'A specific moment in time, such as 2018-07-01T12:00:00-04:00, serialized in ISO8601 format',
   joda.ZonedDateTime,
   joda.ZonedDateTime.parse
 );
-exports.ZoneId = jodaToGraphql(
+export const ZoneId = jodaToGraphql(
   'ZoneId',
   'A timezone ID from the IANA timezone database, such as America/New_York or UTC',
   joda.ZoneId,
